5e0aec1d77a536cffc79dd01578bcf426c15dc88
Mblikolo <rusakov-rv@yandex.ru>
2016-03-19 19:52:37 +0500

    Подключил nHibernate, перевёл AspNet.Identity на nHibernate

--- AccessControlSystem/App_Start/IdentityConfig.cs
+++ AccessControlSystem/App_Start/IdentityConfig.cs
@@ -1,16 +1,16 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Data.Entity;
 using System.Linq;
 using System.Security.Claims;
 using System.Threading.Tasks;
 using System.Web;
 using Microsoft.AspNet.Identity;
-using Microsoft.AspNet.Identity.EntityFramework;
 using Microsoft.AspNet.Identity.Owin;
 using Microsoft.Owin;
 using Microsoft.Owin.Security;
 using AccessControlSystem.Models;
+using NHibernate.AspNet.Identity;
+using NHibernate;
 
 namespace AccessControlSystem
 {
@@ -42,7 +42,7 @@ namespace AccessControlSystem
 
         public static ApplicationUserManager Create(IdentityFactoryOptions<ApplicationUserManager> options, IOwinContext context) 
         {
-            var manager = new ApplicationUserManager(new UserStore<ApplicationUser>(context.Get<ApplicationDbContext>()));
+            var manager = new ApplicationUserManager(new UserStore<ApplicationUser>(context.Get<ISession>()));
             // Configure validation logic for usernames
             manager.UserValidator = new UserValidator<ApplicationUser>(manager)
             {
---
+++ AccessControlSystem/App_Start/NHibernateManager.cs
@@ -0,0 +1,46 @@
+﻿using AccessControlSystem.Models;
+using NHibernate;
+using NHibernate.AspNet.Identity.Helpers;
+using NHibernate.Cfg;
+using NHibernate.Tool.hbm2ddl;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace AccessControlSystem
+{
+    public static class NHibernateManager
+    {
+        private static volatile ISessionFactory _sessionFactory;
+        private static object _lockRoot = new object();
+
+        public static ISession Create()
+        {
+            if(_sessionFactory == null)
+            {
+                lock(_lockRoot)
+                {
+                    if(_sessionFactory == null)
+                        _sessionFactory = CreateSessionFactory();
+                }
+            }
+            return _sessionFactory.OpenSession();
+        }
+
+        private static ISessionFactory CreateSessionFactory()
+        {
+            var myEntities = new[] {
+                typeof(ApplicationUser)
+            };
+
+            var configuration = new Configuration();
+            configuration.Configure();
+            configuration.AddDeserializedMapping(MappingHelper.GetIdentityMappings(myEntities), null);
+
+            new SchemaExport(configuration).Execute(true, true, false);
+
+            return configuration.BuildSessionFactory();
+        }
+    }
+}
