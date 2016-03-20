73b398ff5ce0d72a979d1ef0faa56bfde564ca54
Mblikolo <rusakov-rv@yandex.ru>
2016-03-20 16:07:01 +0500

    дочекин

---
+++ AccessControlSystem/Domain/Vault.cs
@@ -0,0 +1,11 @@
+﻿using NHibernate.AspNet.Identity;
+
+namespace AccessControlSystem.Domain
+{
+    public class Vault
+    {
+        public virtual int Id { get; set; }
+        public virtual string Name { get; set; }
+        public virtual IdentityUser User { get; set; }
+    }
+}
